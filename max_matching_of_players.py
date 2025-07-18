b4d38b16f93f68e63798cf7707c7ed8b03d25728
Saloni Kumari <saloni.kumari12@s.amity.edu>
2025-07-18 23:53:05 +0530

    Add files via upload
    ✅ Day 60: Meeting Room Allocation
    📌 Problem Statement:
    
    You're given:
    
    An integer n → number of meeting rooms labeled 0 to n - 1.
    
    A list meetings, where each element is [start, end] representing a meeting.
    
    🧠 Rules for Room Assignment:
    
    Always assign the meeting to the unused room with the lowest number.
    
    If no room is free, delay the meeting.
    
    The delayed meeting keeps its original duration.
    
    It is scheduled as soon as a room becomes free.
    
    If multiple meetings are delayed, schedule the one with the earliest original start time first.
    
    🎯 Goal:
    
    Return the index of the room that hosted the most number of meetings.
    ⏱️ Time and Space Complexity:
    
    Time Complexity: O(m log n)
    
    Sorting meetings: O(m log m)
    
    Room management via heaps: O(m log n)
    
    Space Complexity: O(n)
    
    For available, ongoing, and count arrays.

---
+++ max_matching_of_players.py
@@ -0,0 +1,16 @@
+def matchPlayersAndTrainers(players, trainers):
+    players.sort()
+    trainers.sort()
+
+    i = j = 0
+    count = 0
+
+    while i < len(players) and j < len(trainers):
+        if players[i] <= trainers[j]:
+            count += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+
+    return count
